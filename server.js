5deb5a8922256c9a5e13b459962afaa4e6625b05
Courtney Van Breda <cmvanbreda91@gmail.com>
2022-09-10 20:52:14 -0400

    feature complete

--- server.js
+++ server.js
@@ -41,7 +41,10 @@ function filterByQuery(query, animalsArray) {
   // return the filtered results:
   return filteredResults;
   }
-
+  function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
 app.get('/api/animals', (req, res) => {
 let results = animals;
   if (req.query) {
@@ -49,7 +52,11 @@ let results = animals;
   }
   res.json(results);
   });
-
+  app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+      if (result) {res.json(result);}
+      else {res.send(404)}
+  });
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
